5ced8511f40961189de64016d9b637a153e2fe61
midezen <victordiya90@gmail.com>
2024-03-26 21:45:24 +0100

    fixed hosting error

--- index.js
+++ index.js
@@ -5,24 +5,21 @@ import cookieParser from "cookie-parser";
 import AuthRoute from "./routes/Auth.js";
 import UserRoute from "./routes/User.js";
 import TaskRoute from "./routes/Task.js";
-import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const router = express.Router();
 
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(cors);
 
 mongoose
   .connect(process.env.mongoUrL)
   .then(() => console.log("db connected"))
   .catch((err) => console.log("db is not connected. This is the" + err));
 
-router.get("/health", (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({ status: "UP" });
 });
 
