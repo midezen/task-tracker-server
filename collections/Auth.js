12f94da29abb7b95a87ac78f62c9be50a81f35b9
midezen <victordiya90@gmail.com>
2024-03-28 15:05:30 +0100

    commit

--- collections/Auth.js
+++ collections/Auth.js
@@ -60,7 +60,7 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   try {
     res
-      .clearCookie("yabatech/access_token", { sameSite: "none", secure: true })
+      .clearCookie("access_token", { sameSite: "none", secure: true })
       .status(200)
       .json("You've logged out successfully");
   } catch (err) {
