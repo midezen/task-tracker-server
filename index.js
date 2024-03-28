85662e59e04ee01990d0bc04a6b7ac1ab07224dc
midezen <victordiya90@gmail.com>
2024-03-28 16:28:52 +0100

    fixed cookies issue

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(process.env.mongoUrL)
