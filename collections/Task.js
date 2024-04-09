5cfe3e554228b3ece8fd615a55b6f6e7e9ab18d7
midezen <victordiya90@gmail.com>
2024-04-09 12:47:37 +0100

    commit

---
+++ collections/Task.js
@@ -0,0 +1,143 @@
+import Task from "../models/Task.js";
+import User from "../models/User.js";
+import nodecron from "node-cron";
+import { sendTaskNotification } from "../notification.js";
+
+export const createTask = async (req, res) => {
+  try {
+    const newTask = new Task({
+      taskName: req.body.taskName,
+      description: req.body.description,
+      assignedUser: req.body.assignedUser,
+      deadLine: req.body.deadLine,
+      taskStatus: req.body.taskStatus,
+      assignedBy: req.body.assignedBy,
+      priority: req.body.priority,
+    });
+
+    const theAssignedUser = await User.findOne({
+      username: newTask.assignedUser,
+    });
+
+    !theAssignedUser && res.status(404).json("This user does not exist");
+    const savedTask = await newTask.save();
+    theAssignedUser.tasks.push(savedTask);
+    theAssignedUser.save();
+
+    sendTaskNotification(
+      "adenirandaniel565@gmail",
+      "adenirandaniel575@gmail.com",
+      "not started"
+    );
+    //since it is automated, we will now use crom
+    // Schedule a cron job to run every day
+    nodecron.schedule("0 0 * * *", async () => {
+      // Get all tasks from the database
+      const tasks = await Task.find();
+
+      // Calculate the reminder date (one day before the deadline) for each task
+      tasks.forEach((task) => {
+        const deadline = new Date(task.deadLine);
+        const reminderDate = new Date(deadline);
+        reminderDate.setDate(reminderDate.getDate() - 1);
+
+        // Check if the current date matches the reminder date
+        const currentDate = new Date();
+        if (
+          currentDate.getFullYear() === reminderDate.getFullYear() &&
+          currentDate.getMonth() === reminderDate.getMonth() &&
+          currentDate.getDate() === reminderDate.getDate()
+        ) {
+          // Send the reminder notification to the assigned user
+          sendReminderNotification(
+            task.assignedUser,
+            task.taskName,
+            reminderDate
+          );
+        }
+      });
+    });
+    console.log(theAssignedUser);
+
+    res.status(200).json(savedTask);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    const theAssignedUser = await User.findOne({ username: task.assignedUser });
+    theAssignedUser.tasks.pull(task);
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    theAssignedUser.tasks.push(updatedTask);
+
+    theAssignedUser.save();
+
+    res.status(200).json(updatedTask);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    const taskAssignedUser = await User.findOne({
+      username: task.assignedUser,
+    });
+    taskAssignedUser.tasks.pull(task);
+    taskAssignedUser.save();
+    await Task.findByIdAndDelete(req.params.id);
+
+    res.status(200).json("Successfully deleted task");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const getTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const getTasks = async (req, res) => {
+  try {
+    const completed = req.query.completed;
+    const notStarted = req.query.notStarted;
+    const inProgress = req.query.inProgress;
+    let tasks;
+    if (completed) {
+      tasks = await Task.find({ taskStatus: "completed" }).sort({
+        createdAt: "desc",
+      });
+    } else if (notStarted) {
+      tasks = await Task.find({ taskStatus: "not started" }).sort({
+        createdAt: "desc",
+      });
+    } else if (inProgress) {
+      tasks = await Task.find({ taskStatus: "in progress" }).sort({
+        createdAt: "desc",
+      });
+    } else {
+      tasks = await Task.find().sort({ createdAt: "desc" });
+    }
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const getStaffToNotify = () => {};
---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import AuthRoute from "./routes/Auth.js";
+import UserRoute from "./routes/User.js";
+import TaskRoute from "./routes/Task.js";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: "https://dazzling-praline-5c3ff0.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"], // Example methods you may want to allow
+};
+
+// Use cors with the above options for all routes
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // Specific origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  ); // Include other headers you want to allow
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Include other methods you want to allow
+  next();
+});
+
+mongoose
+  .connect(process.env.mongoUrL)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log("db is not connected. This is the" + err));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "UP" });
+});
+
+app.use("/api/auth", AuthRoute);
+app.use("/api/user", UserRoute);
+app.use("/api/task", TaskRoute);
+
+app.listen(8000 || process.env.PORT, () => {
+  console.log("app is connected and listening on port 8000");
+});
