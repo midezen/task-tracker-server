38992708d1e545d834fdb217b55875031051ee38
midezen <victordiya90@gmail.com>
2024-04-09 12:18:42 +0100

    commit

--- models/Task.js
+++ models/Task.js
@@ -4,8 +4,12 @@ const TaskSchema = new mongoose.Schema(
   {
     taskName: { type: String, require: true, unique: true },
     description: { type: String },
+    assignedBy: { type: String, require: true },
     assignedUser: { type: String, require: true },
+    assignedBy: { type: String, require: true },
+    Priority: { type: String, require: true },   
     deadLine: { type: String, require: true },
+    priority: { type: String, require: true },
     taskStatus: { type: String, default: "not started" },
   },
   { timestamps: true }
